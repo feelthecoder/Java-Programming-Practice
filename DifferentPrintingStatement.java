8bb009c448a9d94bfdcd203794803e3adba0c0a9
Vikas Kumar <vikaskumar20172210@gmail.com>
2019-07-01 10:56:53 +0530

    Screen Output methods
    WAP to illustrate difference between print(),println(),printf() method?

---
+++ DifferentPrintingStatement.java
@@ -0,0 +1,13 @@
+package assignment;
+
+public class DifferentPrintingStatement {
+
+	public static void main(String[] args) {
+		int x=97;
+		System.out.println(x);
+		System.out.print(x);
+		System.out.printf("%c",x);
+
+	}
+
+}
